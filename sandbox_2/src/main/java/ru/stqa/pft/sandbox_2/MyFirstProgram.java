7fe96d45ea5b56d98c636dc07dc7944d12be06a9
A_Bot <a_bot@github.com>
2018-08-13 21:14:41 -0400

    1.9

--- sandbox_2/src/main/java/ru/stqa/pft/sandbox_2/MyFirstProgram.java
+++ sandbox_2/src/main/java/ru/stqa/pft/sandbox_2/MyFirstProgram.java
@@ -8,18 +8,10 @@ public class MyFirstProgram {
         hello("Alex");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4,6);
-        System.out.println("Площадь квадрата со стороной " + r.a + " и " + r.b + " = " + area(r));
-    }
-
-    public static double area(Square s) {
-        return s.l * s.l;
-    }
-
-    public static double area(Rectangle r) {
-        return r.a * r.b;
+        System.out.println("Площадь квадрата со стороной " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {
