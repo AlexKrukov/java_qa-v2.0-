60f4cda7ec344bf2a1d71a8c2ff2ea7b56f3e63a
A_Bot <a_bot@github.com>
2018-08-21 22:09:06 -0400

    2.9 No recorder test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,7 +7,7 @@ import ru.stqa.pft.addressbook.model.ContactData;
 public class ContactCreationTests extends TestBase {
 
     @Test
-    public void tesContactCreation() {
+    public void testContactCreation() {
         app.getContactHelper().initContactCreation();
         app.getContactHelper().fillContactForm(new ContactData("Alex", "Krukov", "222333444", "222333@gmail.com"));
         app.getContactHelper().submitContactCreation();
