9c2634984c0846302a119c6b2d8bcf1d00d88d7f
Alex Krukov <alexkrukov026@gmail.com>
2018-09-05 22:19:29 -0400

    3.8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -9,7 +9,7 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() {
         app.getContactHelper().initContactCreation();
-        app.getContactHelper().fillContactForm(new ContactData("Alex", "Krukov", "222333444", "222333@gmail.com"));
+        app.getContactHelper().fillContactForm(new ContactData("Alex", "Krukov", "222333444", "222333@gmail.com", "test1"), true);
         app.getContactHelper().submitContactCreation();
         app.getContactHelper().returnToContactPage();
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -9,7 +9,7 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification(){
         app.getContactHelper().initContactModification();
-        app.getContactHelper().fillContactForm(new ContactData("Alex", "Krukov", "222333444", "222333@gmail.com"));
+        app.getContactHelper().fillContactForm(new ContactData("Alex", "Krukov", "222333444", "222333@gmail.com", null), false);
         app.getContactHelper().updateModifiedContact();
         app.getContactHelper().returnToContactPage();
     }
